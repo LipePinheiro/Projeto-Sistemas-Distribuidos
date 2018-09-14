3f01b09ea90e66992273c179292d0518050ec0b5
LipePinheiro <filipemanuel-pinheiro@hotmail.com>
2018-09-14 11:37:59 +0100

    Projeto

---
+++ Projeto SD/ExerciciosProgramacao/src/com/exercicios/model/Solutions.java
@@ -0,0 +1,38 @@
+package com.exercicios.model;
+
+public class Solutions {
+
+	public String code;
+	public String language;
+	public String output;
+	public Solutions() {};
+	
+	public Solutions(String code, String language, String output) {
+		super();
+		this.code = code;
+		this.language = language;
+		this.output = output;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+}
+
+	public String getOutput() {
+		return output;
+	}
+
+	public void setOutput(String output) {
+		this.output = output;
+}
+
+}
